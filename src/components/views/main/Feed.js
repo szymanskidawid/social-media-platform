ba00ceca94d9043ea6f75a38ba595ae9210e3805
Dawid Szymański <dawidszymanski.dev@outlook.com>
2024-07-26 12:15:22 +0200

    new components

---
+++ src/components/views/main/Feed.js
@@ -0,0 +1,15 @@
+import Post from "../../post/Post"
+
+const Feed = () => {
+  return (
+    <div className="feed-container">
+      <Post />
+      <Post />
+      <Post />
+      <Post />
+      <Post />
+    </div>
+  )
+}
+
+export default Feed
