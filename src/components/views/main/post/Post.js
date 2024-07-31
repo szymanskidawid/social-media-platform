bcd6538c756463cf59cc6de72c1df2869a48aee6
Dawid Szymański <dawidszymanski.dev@outlook.com>
2024-08-01 00:34:49 +0200

    implemented animation to comments section

--- src/components/views/main/post/Post.js
+++ src/components/views/main/post/Post.js
@@ -1,6 +1,11 @@
+import { useState } from "react";
 import UserInfo from "../../../small-components/UserInfo";
+import InputField from "../../../small-components/InputField";
+import MainSectionButton from "../MainPageButton";
 
 const Post = () => {
+  const [commentsOpen, setCommentsOpen] = useState(false);
+
   return (
     <div className="post-container">
       <div className="post-top-section">
@@ -9,13 +14,33 @@ const Post = () => {
         </div>
         <div className="post-top-time-posted">Posted 12 hours ago</div>
       </div>
-      <div className="post-top-description">Description</div>
-      <div className="post-image">Image</div>
-      <div className="post-bot-section">
-        <div className="post-bot-like">Like</div>
-        <div className="post-bot-comment">Comment</div>
+      <div className="post-top-description">Look at my new picture!</div>
+      <div className="post-image">
+        <img src="https://picsum.photos/id/237/1000/800" />
       </div>
-      <div className="post-comments-section">Comments Section</div>
+      <div className="post-bot-section">
+        <div className="post-bot-like">
+          <div className="post-bot-like-counter">0</div>
+          <i class="icon fa-solid fa-thumbs-up fa-xl"></i>
+        </div>
+        <div className="post-bot-comment">
+          <div className="post-bot-comment-text">Comment</div>
+          <i
+            class="icon fa-solid fa-comment fa-xl"
+            onClick={() => setCommentsOpen(!commentsOpen)}
+          ></i>
+        </div>
+      </div>
+      <div className={`post-comments-container ${commentsOpen ? "open" : ""}`}>
+        <div className="post-comments">Comments</div>
+        <div className="post-comments-bot">
+          <InputField
+            style={{ width: "100%" }}
+            placeholder={"Write a comment..."}
+          />
+          <MainSectionButton text={"Send"} />
+        </div>
+      </div>
     </div>
   );
 };
