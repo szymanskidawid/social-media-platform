362f5fe62c0ece50bf7ae7c1ad66c28e6236bc4e
Dawid Szymański <dawidszymanski.dev@outlook.com>
2024-08-02 14:27:42 +0200

    added variety to Feed display

--- src/components/views/main/Feed.js
+++ src/components/views/main/Feed.js
@@ -1,10 +1,13 @@
+import { useContext } from "react";
 import CreatePost from "./post/CreatePost";
 import Post from "./post/Post";
+import { MainViewContext } from "../../../contexts/MainViewContext";
 
 const Feed = () => {
+  const { mainView } = useContext(MainViewContext);
   return (
     <div className="feed-container">
-      <CreatePost />
+      {mainView === "mainFeed" ? <CreatePost /> : ""}
       <Post />
       <Post />
       <Post />
