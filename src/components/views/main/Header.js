de5cb51eec99bfb078459116d311de47896ce86b
Dawid Szymański <dawidszymanski.dev@outlook.com>
2025-04-27 00:21:44 +0200

    added counter for new notifications

--- src/components/views/main/Header.js
+++ src/components/views/main/Header.js
@@ -1,12 +1,15 @@
 import { useContext } from "react";
 import Logo from "../../small-components/Logo";
-import { ContainerViewContext } from "../../../contexts/ContainerViewContext";
 import InputField from "../../small-components/InputField";
 import { LightModeContext } from "../../../contexts/LightModeContext";
+import { DataContext } from "../../../contexts/DataContext";
+import { ContainerViewContext } from "../../../contexts/ContainerViewContext";
 import { useNavigate } from "react-router-dom";
+import { Badge } from "@mui/material";
 
 const Header = () => {
   const { isLightMode } = useContext(LightModeContext);
+  const { user, notifications } = useContext(DataContext);
   const { setContainerView } = useContext(ContainerViewContext);
 
   const navigate = useNavigate();
@@ -29,10 +32,20 @@ const Header = () => {
           className={`icon-${isLightMode ? "light-mode" : "dark-mode"} fa-solid fa-comment-dots fa-2xl`}
           onClick={() => setContainerView("chats")}
         />
-        <i
-          className={`icon-${isLightMode ? "light-mode" : "dark-mode"} fa-solid fa-circle-exclamation fa-2xl`}
-          onClick={() => setContainerView("notifications")}
-        />
+        <Badge
+          badgeContent={
+            notifications.filter(
+              (notification) => notification.notified_user_id === user._id
+            ).length
+          }
+          color="error"
+          showZero={false}
+        >
+          <i
+            className={`icon-${isLightMode ? "light-mode" : "dark-mode"} fa-solid fa-circle-exclamation fa-2xl`}
+            onClick={() => setContainerView("notifications")}
+          />
+        </Badge>
         <i
           className={`icon-${isLightMode ? "light-mode" : "dark-mode"} fa-solid fa-gear fa-2xl`}
           onClick={() => setContainerView("options")}
